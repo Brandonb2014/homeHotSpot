9ebd884926cf1ab252002c3c6b477bbcec9867eb
Brandon <brandon.birschbach@gmail.com>
2020-03-16 00:16:04 -0600

    Adding JSON data.

---
+++ listings.js
@@ -0,0 +1,44 @@
+populateListings([
+  {
+    "image_url"  :  "images/listings/lovendahl/pic1.jpg",
+    "nmls_url"   :  "https://utahrealestate.com/1655368",
+    "price"      :  "310,000",
+    "status"     :  "Under Contract",
+    "street"     :  "2929 S Carla St",
+    "city"       :  "West Valley City",
+    "state"      :  "UT",
+    "zip"        :  "84120",
+    "beds"       :  "4",
+    "baths"      :  "2",
+    "sqft"       :  "1,520",
+    "acres"      :  "0.15"
+  },
+  {
+    "image_url"  :  "images/listings/pic1.jpg",
+    "nmls_url"   :  "https://myextraland.com/wordpress/?product=1-01-acre-wanship-ut-parkview-rd-rockport-est",
+    "price"      :  "34,000",
+    "status"     :  "Active",
+    "street"     :  "Parkview Rd Rockport Estates",
+    "city"       :  "Wanship",
+    "state"      :  "UT",
+    "zip"        :  "",
+    "beds"       :  "-",
+    "baths"      :  "-",
+    "sqft"       :  "-",
+    "acres"      :  "1.01"
+  },
+  {
+    "image_url"  :  "images/listings/Website-comingsoon2.png",
+    "nmls_url"   :  "#contact-section",
+    "price"      :  "---,---",
+    "status"     :  "Active",
+    "street"     :  "123 Your Home Here",
+    "city"       :  "",
+    "state"      :  "UT",
+    "zip"        :  "",
+    "beds"       :  "-",
+    "baths"      :  "-",
+    "sqft"       :  "-",
+    "acres"      :  "-"
+  }
+])
